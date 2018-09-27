03e2102daf510d1b5adec522fc05ea7f3204795c
xiaoqiaoWEB <346043591@qq.com>
2018-09-27 16:08:08 +0800

    webpack vue css less

---
+++ webpack-vue-todo/src/app.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <div class="text">
+            this is text asfasfasfasf
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "app",
+        data(){
+            return{
+                text:'abc'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.text{
+    color: lawngreen;
+    font-size: 30px;
+    font-weight: bold;
+}
+#app{
+    background: url("./assets/img/bg.jpeg") no-repeat;
+    background-size: cover;
+    width: 100%;
+    height: 100vh;
+}
+</style>
